0c8f58668abf684fa7952be2e7e82e83d51084b1
malukaum <otavio.a.chaves@gmail.com>
2018-11-10 18:32:47 -0200

    Criação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AppService} from "./app.service";
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [AppService]
 })
 export class AppRoutingModule { }
